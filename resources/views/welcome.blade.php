2a52c1875665c7bf89c0bd7d6544181c215c2228
Tim Romeijn <tim28@live.nl>
2018-04-04 16:10:13 +0200

    Laravel setup and start
    Laravel setup and start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+
+<head>
+
+    <title></title>
+
+</head>
+
+
+
+<body>
+
+    <h1>Hello World!</h1>
+
+    <p>Eerste setup, later verder</p>
+
+
+</body>
+
+
+
+
+
+
+</html>
