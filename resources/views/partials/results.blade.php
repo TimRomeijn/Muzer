e4c561d0cfad95db3477e0829da16cfbaaf1b118
Tim Romeijn <tim28@live.nl>
2018-05-02 14:39:27 +0200

    Added profile pic to filter results
    Added profile pic to filter results

--- resources/views/partials/results.blade.php
+++ resources/views/partials/results.blade.php
@@ -15,14 +15,19 @@
         {{--<div class="tab-pane fade" id="muzikant" role="tabpanel" aria-labelledby="home-tab">--}}
 
             @if($isMusician)
-                <div class="card">
-                    <div class="card-header">
-                        Naam
+                <div class="card-group">
+                    <div class="card col-md-4 d-none d-sm-block">
+                        <img src="https://picsum.photos/500/?random" alt="profile-picture" class="card-img result-profile-pic">
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Muzikant</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="/profiles/musicianprofile/" class="btn btn-primary">Bekijk</a>
+                    <div class="card col-sm-8">
+                        <div class="card-header">
+                            Naam
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Muzikant</h5>
+                            <p class="card-text">Wat snelle informatie over het muzikantenprofiel.</p>
+                            <a href="/profiles/musicianprofile/" class="btn btn-primary">Bekijk</a>
+                        </div>
                     </div>
                 </div>
             @endif
@@ -31,14 +36,19 @@
         {{--<div class="tab-pane fade" id="band" role="tabpanel" aria-labelledby="profile-tab">--}}
 
             @if($isBand)
-                <div class="card">
-                    <div class="card-header">
-                        Naam
+                <div class="card-group">
+                    <div class="card col-md-4 d-none d-sm-block">
+                        <img src="https://picsum.photos/500/?random" alt="profile-picture" class="card-img result-profile-pic">
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Band</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="/profiles/bandprofile/" class="btn btn-primary">Bekijk</a>
+                    <div class="card col-sm-8">
+                        <div class="card-header">
+                            Naam
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Band</h5>
+                            <p class="card-text">Wat snelle informatie over het bandprofiel.</p>
+                            <a href="/profiles/musicianprofile/" class="btn btn-primary">Bekijk</a>
+                        </div>
                     </div>
                 </div>
             @endif
@@ -47,14 +57,19 @@
         {{--<div class="tab-pane fade" id="podium" role="tabpanel" aria-labelledby="contact-tab">--}}
 
             @if($isStage)
-                <div class="card">
-                    <div class="card-header">
-                        Naam
+                <div class="card-group">
+                    <div class="card col-md-4 d-none d-sm-block">
+                        <img src="https://picsum.photos/500/?random" alt="profile-picture" class="card-img result-profile-pic">
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Podium</h5>
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="/profiles/stageprofile/" class="btn btn-primary">Bekijk</a>
+                    <div class="card col-sm-8">
+                        <div class="card-header">
+                            Naam
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Podium</h5>
+                            <p class="card-text">Wat snelle informatie over het podiumprofiel.</p>
+                            <a href="/profiles/musicianprofile/" class="btn btn-primary">Bekijk</a>
+                        </div>
                     </div>
                 </div>
             @endif
